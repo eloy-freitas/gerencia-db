a5d3f12c26abc5b0de8270d4e517f6e028183b1a
eloy-freitas <freitaseloy.1@gmail.com>
2021-07-01 09:35:22 -0300

    organização das pastas e questão 1 do exercicio 2

---
+++ exercicio_2/exercicio_2.sql
@@ -0,0 +1,128 @@
+CREATE DATABASE exercicio_2;
+
+use exercicio_2;
+
+CREATE TABLE Cidade
+(
+	CodCidade	INTEGER NOT NULL,
+	Nome		VARCHAR(100) NOT NULL,
+	UF		CHAR(1),
+	CONSTRAINT pkCidade PRIMARY KEY (CodCidade)
+);
+
+CREATE TABLE Clube
+(
+	CodClube	INTEGER NOT NULL,
+	Nome		VARCHAR(100) NOT NULL,
+	CodCidade	INTEGER NOT NULL,
+	CONSTRAINT pkClube PRIMARY KEY (CodClube),
+	CONSTRAINT fkClubeCidade FOREIGN KEY (CodCidade) REFERENCES Cidade(CodCidade)
+);
+
+CREATE TABLE Campeonato
+(
+	CodCampeonato	INTEGER NOT NULL,
+	Descricao	VARCHAR(100),
+	CONSTRAINT pkCampeonato PRIMARY KEY (CodCampeonato)
+);
+
+CREATE TABLE ClubeCampeonato
+(
+	CodClube	INTEGER NOT NULL,
+	CodCampeonato 	INTEGER NOT NULL,
+	Ano		INTEGER NOT NULL,
+	Pontos		INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT pkClubeCampeonato PRIMARY KEY (CodClube, CodCampeonato, Ano),
+	CONSTRAINT fkClubeCampeonatoCampeonato FOREIGN KEY (CodCampeonato) REFERENCES Campeonato(CodCampeonato),
+	CONSTRAINT fkClubeCampeonatoClube FOREIGN KEY (CodClube) REFERENCES Clube(CodClube)
+);
+
+CREATE TABLE Jogador
+(
+	CodJogador	INTEGER NOT NULL,
+	Nome		VARCHAR(100) NOT NULL,
+	DataNascimento	DATE,	
+	ValorPasse	FLOAT DEFAULT 0 NOT NULL,
+	CONSTRAINT pkJogador PRIMARY KEY (CodJogador)
+);
+
+CREATE TABLE JogadorClubeCampeonato
+(
+	CodClube	INTEGER NOT NULL,
+	CodCampeonato	INTEGER NOT NULL,
+	Ano				INTEGER NOT NULL,
+	CodJogador	INTEGER NOT NULL,	
+	Gols		INTEGER DEFAULT 0 NOT NULL,	
+	CONSTRAINT pkJogadorClubeCampeonato PRIMARY KEY (CodClube, CodCampeonato, Ano, CodJogador),
+	CONSTRAINT fkJogadorClubeCampeonato FOREIGN KEY (CodClube, CodCampeonato, Ano) REFERENCES ClubeCampeonato (CodClube, CodCampeonato, Ano),
+	CONSTRAINT fkJogadorCCJogador	FOREIGN KEY (CodJogador) REFERENCES Jogador(CodJogador)	
+);
+
+CREATE TABLE Estadio
+(
+	CodEstadio	INTEGER NOT NULL,
+	Nome		VARCHAR(100) NOT NULL,
+	CodCidade	INTEGER NOT NULL,
+	Capacidade	INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT pkEstadio PRIMARY KEY (CodEstadio),
+	CONSTRAINT fkEstadioCidade FOREIGN KEY (CodCidade) REFERENCES Cidade(CodCidade)
+);
+
+CREATE TABLE Partida
+(
+	CodCampeonato	INTEGER NOT NULL,
+	Ano		INTEGER	NOT NULL,
+	CodPartida	INTEGER NOT NULL,
+	DataHora	TIMESTAMP NOT NULL,
+	CodEstadio	INTEGER NOT NULL,
+	CodClube1	INTEGER NOT NULL,
+	GolsClube1	INTEGER DEFAULT 0 NOT NULL,
+	CodClube2	INTEGER NOT NULL,
+	GolsClube2	INTEGER DEFAULT 0 NOT NULL,
+	CONSTRAINT pkPartida	PRIMARY KEY (CodCampeonato, Ano, CodPartida),
+	CONSTRAINT fkPartidaCampeonato FOREIGN KEY (CodCampeonato) REFERENCES Campeonato(CodCampeonato),
+	CONSTRAINT fkPartidaEstadio FOREIGN KEY (CodEstadio) REFERENCES Estadio(CodEstadio),
+	CONSTRAINT fkPartidaClube1	FOREIGN KEY (CodClube1) REFERENCES Clube(CodClube),
+	CONSTRAINT fkPartidaClube2	FOREIGN KEY (CodClube2) REFERENCES Clube(CodClube)
+);
+
+INSERT INTO Cidade (CodCidade, Nome, UF) VALUES 
+(1, 'Linhares', 'E'),
+(2, 'Vitória', 'E');
+
+INSERT INTO Estadio (CodEstadio, Nome, CodCidade, Capacidade)
+VALUES (1, 'Clube do América', 1, 60000);
+
+INSERT INTO Clube (CodClube, Nome, CodCidade) VALUES 
+(1, 'Linhares esporte clube', 1),
+(2, 'Desportiva', 2);
+
+INSERT INTO Campeonato (CodCampeonato, Descricao)
+VALUES (1, 'Capixabão');
+
+INSERT INTO ClubeCampeonato (CodClube, CodCampeonato, Ano, Pontos)
+VALUES 
+(1, 1, 2021, 0),
+(2, 1, 2021, 0);
+
+
+INSERT INTO Partida 
+(CodCampeonato, Ano, CodPartida, DataHora, CodEstadio, CodClube1, GolsClube1, CodClube2, GolsClube2)
+VALUES
+(1, 2021, 1, now(), 1, 1, 3, 2, 0);
+
+INSERT INTO Partida 
+(CodCampeonato, Ano, CodPartida, DataHora, CodEstadio, CodClube1, GolsClube1, CodClube2, GolsClube2)
+VALUES
+(1, 2021, 2, now(), 1, 1, 1, 2, 1);
+
+INSERT INTO Partida 
+(CodCampeonato, Ano, CodPartida, DataHora, CodEstadio, CodClube1, GolsClube1, CodClube2, GolsClube2)
+VALUES
+(1, 2021, 3, now(), 1, 1, 1, 2, 2);
+
+DELETE FROM Partida WHERE CodPartida = 2;
+
+UPDATE Partida 
+SET GolsClube2 = 1
+WHERE CodPartida = 3;
---
+++ exercicio_2/triggers.sql
@@ -0,0 +1,143 @@
+DELIMITER |
+CREATE TRIGGER tInsertPartida
+AFTER INSERT ON Partida
+FOR EACH ROW 
+BEGIN 
+	IF (NEW.GolsClube1 > NEW.GolsClube2) THEN 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 3
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube1 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+	ELSEIF (NEW.GolsClube2 > NEW.GolsClube1) THEN
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 3
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube2 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+	ELSE 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 1
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube1 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+		
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 1
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube2 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+	END IF;
+END;
+|
+
+DELIMITER |
+CREATE TRIGGER tUpdatePartida
+AFTER UPDATE ON Partida
+FOR EACH ROW 
+BEGIN 
+	IF (OLD.GolsClube1 > OLD.GolsClube2) THEN 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 3
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube1 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+	ELSEIF (OLD.GolsClube2 > OLD.GolsClube1) THEN
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 3
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube2 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+	ELSE 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 1
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube1 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+		
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 1
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube2 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+	END IF;
+
+	IF (NEW.GolsClube1 > NEW.GolsClube2) THEN 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 3
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube1 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+	ELSEIF (NEW.GolsClube2 > NEW.GolsClube1) THEN
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 3
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube2 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+	ELSE 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 1
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube1 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+		
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos + 1
+		WHERE 
+			NEW.CodCampeonato = cc.CodCampeonato 
+			AND NEW.CodClube2 = cc.CodClube 
+			AND NEW.Ano = cc.Ano;
+	END IF;
+
+END;
+|
+
+DELIMITER |
+CREATE TRIGGER tDeletePartida
+BEFORE DELETE ON Partida
+FOR EACH ROW 
+BEGIN 
+	IF (OLD.GolsClube1 > OLD.GolsClube2) THEN 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 3
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube1 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+	ELSEIF (OLD.GolsClube2 > OLD.GolsClube1) THEN
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 3
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube2 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+	ELSE 
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 1
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube1 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+		
+		UPDATE ClubeCampeonato cc 
+		SET cc.Pontos = cc.Pontos - 1
+		WHERE 
+			OLD.CodCampeonato = cc.CodCampeonato 
+			AND OLD.CodClube2 = cc.CodClube 
+			AND OLD.Ano = cc.Ano;
+	END IF;
+END;
+|
+
+DROP TRIGGER  tInsertPartida;
